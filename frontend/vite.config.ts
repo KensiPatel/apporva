38dfdc095c05f7b3f35646c65ea9d78425ee35fe
adistrim <adistrim.dev@zohomail.in>
2026-02-25 21:04:30 +0530

    fix: routeGen issue

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import router from '@tanstack/router-plugin/vite'
 
 const config = defineConfig({
   plugins: [
+    router(),
     tsconfigPaths({ projects: ['./tsconfig.json'] }),
     tailwindcss(),
     viteReact(),
