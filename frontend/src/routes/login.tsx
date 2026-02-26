41020e108f31eb610869ab8ee24809db57b09c30
maitrilad <maitrilad1716@gmail.com>
2026-02-26 17:43:28 +0530

    WIP: login UI setup

--- frontend/src/routes/login.tsx
+++ frontend/src/routes/login.tsx
@@ -1,9 +1,60 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { useState } from "react"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button"
+
 
 export const Route = createFileRoute('/login')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/login"!</div>
+  const[email,setEmail]= useState("")
+  const [password, setPassword] = useState("")
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    console.log("Email:", email)
+    console.log("Password:", password)
+  }
+
+  return (
+    <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>Login</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          
+          <div>
+            <Label>Email</Label>
+            <Input
+              type="email"
+              placeholder="abc@projectapprova.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div>
+            <Label>Password</Label>
+            <Input
+              type="password"
+              placeholder="••••••••"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />            
+          </div>
+
+          <Button type="submit" className="w-full">
+            Sign In
+          </Button>
+
+        </form>
+      </CardContent>
+    </Card>
+  )
 }
