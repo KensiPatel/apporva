dda586f17dbb4a0d1c40a7728ec7e5fc993c156e
maitrilad <maitrilad1716@gmail.com>
2026-03-05 02:23:06 +0530

    feat(auth): add signup flow and protect home route

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -3,6 +3,7 @@ import { createFileRoute } from '@tanstack/react-router'
 import { useQuery, useMutation } from "@tanstack/react-query"
 import { getMe, logoutUser } from "@/lib/api"
 import { useNavigate } from "@tanstack/react-router"
+import { useEffect } from "react"
 
 export const Route = createFileRoute('/')({
   component: RouteComponent,
@@ -21,22 +22,25 @@ const logoutMutation= useMutation({
     navigate({to: "/login"})
   }
 })
-if (isLoading) return <div>Loading...</div>
 
+useEffect(() => {
   if (isError) {
     navigate({ to: "/login" })
-    return null
   }
-  return (
-    <div className="flex flex-col space-y-10 h-screen w-screen items-center justify-center">
-      <h1>Approva</h1>
+}, [isError, navigate])
 
-      <Button
-      onClick={() => logoutMutation.mutate()}
-      disabled={logoutMutation.isPending}
-      >
-        {logoutMutation.isPending ? "Logging out..." : "Logout"}
-      </Button>
-    </div>
+if (isLoading) return <div>Loading...</div> 
+
+return (
+  <div className="flex flex-col space-y-10 h-screen w-screen items-center justify-center">
+    <h1>Approva</h1>
+
+    <Button
+     onClick={() => logoutMutation.mutate()}
+    disabled={logoutMutation.isPending}
+    >
+      {logoutMutation.isPending ? "Logging out..." : "Logout"}
+    </Button>
+  </div>
   )
 }
